fc11c2aeffc9357a1c01466135b46d6551bf0a96
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2024-01-19 11:22:45 +0100

    Added Surface type, and plain(), slope() func

--- surfaces.h
+++ surfaces.h
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <functional>
 
+constexpr Real STEP_HEIGHT = 1;
+
 class Point
 {
 public:
@@ -14,7 +16,6 @@ public:
 
     friend std::ostream &operator<<(std::ostream &out, const Point &p);
 
-private:
     const Real x;
     const Real y;
 };
@@ -24,5 +25,34 @@ std::ostream &operator<<(std::ostream &out, const Point &p)
     out << p.x << " " << p.y;
     return out;
 }
+
+/**
+ *  Maybe function should get Point via reference???
+*/
+using Surface = std::function<Real(Point)>;
+
+/**
+ * Generates plain f(x, y) = 0;
+*/
+Surface plain()
+{
+    // We want to stress that returned value is of type REAL.
+    return [](const Point &p) -> Real {return 0;};
+}
+
+/**
+ * Generates slope f(x, y) = x;
+*/
+Surface slope()
+{
+    return [](const Point &p) -> Real {return p.x;};
+}
+
+Surface steps(Real s = 1)
+{
+    static const Real step_height = STEP_HEIGHT;
+     
+}
+
 
 #endif
